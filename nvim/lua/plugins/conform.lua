2420789ec3b566b50f61e0ee04e3d686aa90f3fe
Cercel, Cristian <cristian.cercel@outlook.com>
2026-08-06 19:10:07 -0400

    added conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                timeout_ms = 5000,
+                lsp_format = "fallback",
+            },
+            formatters_by_ft = {
+                lua = { "stylua" },
+                go = { "gofmt" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                json = { "prettier" },
+            },
+        })
+    end,
+}
