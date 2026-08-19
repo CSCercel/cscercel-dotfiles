e9c53426f37329f16c4e0e9a4c754d9d6aa388ab
Cristian Cercel <cristian.cercel@outlook.com>
2026-08-19 12:59:33 -0400

    removed lualine

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -12,18 +12,4 @@ return {
             vim.cmd("colorscheme rose-pine-moon")
         end
     },
-
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require('lualine').setup({
-                options = {
-                    theme = "onedark",
-                },
-            })
-        end
-    },
 }
