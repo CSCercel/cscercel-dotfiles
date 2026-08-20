772268a2ca1e450aae889c7f096a4cccf724efe7
Cristian Cercel <cristian.cercel@outlook.com>
2026-08-19 23:21:59 -0400

    discovered hyprland

---
+++ hypr/config/colors.lua
@@ -0,0 +1,19 @@
+-- Cachy colors
+
+CACHYLGREEN  = "rgba(82dcccff)"
+CACHYMGREEN  = "rgba(00aa84ff)"
+CACHYDGREEN  = "rgba(007d6fff)"
+CACHYLBLUE   = "rgba(01ccffff)"
+CACHYMBLUE   = "rgba(182545ff)"
+CACHYDBLUE   = "rgba(111826ff)"
+CACHYWHITE   = "rgba(ffffffff)"
+CACHYGREY    = "rgba(ddddddff)"
+CACHYGRAY    = "rgba(798bb2ff)"
+
+-- My colors
+CACHYLRED    = "rgba(ff4766ff)"
+CACHYMRED    = "rgba(ff3355ff)"
+CACHYDRED    = "rgba(aa1133ff)"
+CACHYLYELLOW = "rgba(ffb833ff)"
+CACHYMYELLOW = "rgba(ff9900ff)"
+CACHYDYELLOW = "rgba(cc7a00ff)"
---
+++ hypr/config/decorations.lua
@@ -0,0 +1,44 @@
+-- Look and feel configuration
+
+hl.config({
+    general = {
+        gaps_in = 3,
+        gaps_out = 8,
+        border_size = 2,
+        extend_border_grab_area = 10,
+        resize_on_border = true,
+        col = {
+            active_border = {
+                colors = { CACHYLRED, CACHYDRED },
+                angle = 45,
+            },
+            inactive_border = CACHYGRAY,
+        },
+    },
+    group = {
+        col = {
+            border_active = CACHYLBLUE,
+            border_inactive = CACHYGRAY,
+            border_locked_active = CACHYDYELLOW,
+            border_locked_inactive = CACHYGRAY,
+        },
+        groupbar = {
+            col = {
+                active = CACHYLRED,
+                inactive = CACHYGRAY,
+                locked_active = CACHYDYELLOW,
+                locked_inactive = CACHYGRAY,
+            },
+        },
+    },
+    decoration = {
+        dim_special = 0.3,
+        rounding = 10,
+        active_opacity = 1,
+        inactive_opacity = 1,
+        fullscreen_opacity = 1,
+        blur = {
+            enabled = false,
+        },
+    },
+})
